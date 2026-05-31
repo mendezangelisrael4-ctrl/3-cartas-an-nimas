87808073f02a3ef86db9284557cc8f9e71b3ff06
mendezangelisrael4-ctrl <mendezangelisrael4@gmail.com>
2026-05-31 03:01:24 -0600

    3 cartas anonimas (lee desde la terminal)

---
+++ 3_cartas_anonimas.py
@@ -0,0 +1,74 @@
+import sys
+
+def ejecutar_historia():
+    guion = [
+        "\nC.- Mira, ",
+        "de camino a casa me encontré varios papeles tirados cerca de la entrada",
+        "A.- A verrrrr!!!!",
+        "C.- No les veo mucho sentido…",
+        
+        "\n               [ Carta 1 ]               ",
+        "\"Te has vuelto una forma extraña Andrés,",
+        "ni siquiera sé si seguiré llamándote Andrés. ",
+        "Creo que el seguir llamándote por tu nombre rescata los restos de humanidad que (quiero pensar) siguen en ti.\"",
+        
+        "\n               [ Carta 2 ]               ",
+        "\"Andrés, quiero que sepas lo duro que es para nosotros verte así,",
+        "ver como te retuerces,",
+        "verte intentar gritar sin aire,",
+        "me duele ver como los demás no te quieren ayudar por miedo a convertirse en lo que tú eres,",
+        "me duele no querer ayudarte por miedo a volver a convertirme en lo que tú eres,", 
+        "me das miedo, Andrés.\"",
+        
+        "\n               [ Carta 3 ]               ",
+        "\"Hace años dejé de amarte,",
+        "hace años que te olvidé,",
+        "no puedo recordar con detalle cada facción de tu rostro,",
+        "no puedo recordar el sonido de tu voz,",
+        "olvidé la forma en que caminabas,",
+        "olvidé todos tus chistes malos,",
+        "olvidé donde estaban todos tus lunares,",
+        "olvidé cómo te peinabas por las mañanas,",
+        "olvidé todo de ti,",
+        "olvidé como mentir.",
+        "...\"",
+
+        
+        "\n",
+
+        "\nA.- Que pedazo de kk",
+        "C.- ¿Qué te pasa?, pinche obra maestra te estoy leyendo",
+        "A.- ¿Crees que sea real?",
+        "C.- No lo creo, a menos que la cosa en la que se convirtió sea una metáfora de..",
+        
+        "\n...BLA BLA BLA BLA BLA BLA, depresión, BLA BLA BLA BLA BLA BLA …",
+        
+        "\nA.- Pos quien sabe carnal, igual no es pa tanto",
+        "C.- No pienso lo mismo que tú",
+        "A.- ¿Entonces? ",
+        "C.- No sé, yo quiero saber quien las escribió, saber si lo hizo en broma, o si es una cuento o yo qué sé",
+        "Quiero saber más de esto",
+        "A.- Pues no sé, escribe el resto para que no te quedes con la duda",
+        "C.- No creo que se sienta como la historia original.", 
+        "A.- Pues te valga verga, nomás es pa que no te quedes con la duda",
+        "C.- Ta bien",
+
+        "\n",
+
+
+
+
+
+    ]
+    
+    print("\nPresiona ENTER")
+    
+    for linea in guion:
+        input()  
+        print(linea)
+
+if __name__ == "__main__":
+    ejecutar_historia()
+
+
+
